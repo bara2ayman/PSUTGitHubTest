64f333c2047ee5ebbd4ae33510f669883f16d1d5
Firas Ghanim <f.ghanim@psut.edu.jo>
2026-01-07 09:52:24 +0300

    starting with three files for github

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Testing git at PSUT");
+        
+        Person p1= new Person ("Mary", 12);
+        System.out.println (p1.getName ());
+        
+        System.out.println ("Firas");
+    }
+}
